352d0e8c8eb9f7a0a5d59128fbcd6a2048556a35
Sigstus985 <siggeluren@gmail.com>
2025-04-08 12:28:35 +0200

    fixed array rendering

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,35 +1,70 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+	const [buttonVis, setButtonVis] = useState(true);
 	const [formVis, setFormVis] = useState(false);
+	const [formData, setFormData] = useState("");
+	const [todoArray, setTodoArray] = useState([
+		"Pick up the kids at school", "Get drain cleaner"
+	]);
 
 	const handleClick = () => {
+		setButtonVis(false);
 		setFormVis(true);
 	};
-  
-  const submitForm = () => {
-    setFormVis(false)
+
+  const handleChange = (e) => {
+    setFormData(formData)
   }
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setButtonVis(true);
+		setFormVis(false);
+
+    const formData = new FormData(e.target)
+    const payload = Object.fromEntries(formData)
+
+    setTodoArray([...todoArray , payload.title])
+	};
+
+  useEffect(() => {
+    console.log(todoArray)
+  }, [handleSubmit])
 
 	return (
 		<div>
 			<h1>Note it</h1>
 			<aside>
-				<button className="noteBtn" onClick={handleClick}>
-					Note!
-				</button>
+				{buttonVis && (
+					<button className="noteBtn" onClick={handleClick}>
+						Note!
+					</button>
+				)}
 			</aside>
 			{formVis && (
-				<form className="noteForm">
-					<input type="text" placeholder="Note title" />
-          <button onClick={submitForm} >Submit</button>
+				<form className="noteForm" name="noteTitle" onSubmit={handleSubmit}>
+					<input
+						type="text"
+						placeholder="Note title"
+            name="title"
+            onChange={handleChange}
+					/>
+					<button type="submit">Submit</button>
 				</form>
 			)}
 
-			<main className="notes"></main>
+			<main className="notes">
+				{todoArray.map((note) => (
+					<div className="note" key={note}>
+						<h2>{note}</h2>
+					</div>
+				))}
+			</main>
 		</div>
 	);
 }
